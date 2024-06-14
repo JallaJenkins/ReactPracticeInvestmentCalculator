7088e8496c69f12715c636e8447363039d4ab733
JallaJenkins <wakeupalready@gmail.com>
2024-06-13 17:46:46 -0700

    Final final commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,6 @@ import Header from './components/Header.jsx';
 import InputBox from './components/InputBox.jsx';
 import Results from './components/Results.jsx';
 
-import UserInput from './components/UserInput.jsx';
-
 const INITIAL_INVESTMENT_STATE = {
   initialInvestment: 1000,
   annualInvestment: 300,
@@ -15,22 +13,11 @@ const INITIAL_INVESTMENT_STATE = {
 
 function App() {
 
-  // TODO: Consolodate into one object ??
   const [initialInvestment, setInitialInvestment] = useState(1000);
   const [annualInvestment, setAnnualInvestment] = useState(300);
   const [expectedReturn, setExpectedReturn] = useState(5.5);
   const [duration, setDuration] = useState(12);
 
-  const investments = {
-    initialInvestment: initialInvestment,
-    annualInvestment: annualInvestment,
-    expectedReturn: expectedReturn,
-    duration: duration,
-  };
-
-  // const [investmentState, setInvestmentState] = useState(INITIAL_INVESTMENT_STATE);
-
-  // TODO: Move functions and InputBoxes to UserInput ??
   function handleInitialInvestmentChange(event) {
     setInitialInvestment(+event.target.value);
   }
@@ -62,7 +49,12 @@ function App() {
           <InputBox id="duration" label="Duration" defaultValue="12" value={duration} onChange={handleDurationChange} />
         </div>
       </div>
-      <Results investments={investments}
+      <Results investments={{
+        initialInvestment: initialInvestment,
+        annualInvestment: annualInvestment,
+        expectedReturn: expectedReturn,
+        duration: duration,
+      }}
       />
     </>
   );
